6fc6a415a102311b3d73d1f232c30b22175fc43d
Mazen Hassan <mazen.a.hassan99@gmail.com>
2024-04-02 21:14:50 -0700

    modified 2 to 4

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def safe_print_division(a, b):
     """Returns the result of the ivision a/b"""
     try:
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def list_division(mylist1, mylist2, L):
     new = []
     for x in range(L):
